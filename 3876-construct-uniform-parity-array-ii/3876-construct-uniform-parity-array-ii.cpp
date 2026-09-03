6d4f649f9d39d19723616c269c6a9418b8c47bea
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-03 19:45:19 +0530

    Time: 5 ms (54.71%), Space: 165.8 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -3,31 +3,24 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int n= nums1.size() ;
         bool exp =true ;
-        int oddcc = 0, evencc =0 ;
-        int mini =INT_MAX ;
+        int min_even = INT_MAX ;
+        int min_odd =INT_MAX ;
         for(auto num : nums1){
-            mini = min(mini , num) ;
-            if(num %2 ==1){
-                oddcc++ ;
-            }else{evencc++  ;}
+            if(num % 2 ==0){
+                min_even = min(min_even , num ) ;
+            }else {
+                min_odd = min(min_odd , num) ;
+            }
         }
-        if(evencc ==n || oddcc==n){
-            return true ;
-        }
-        int parity ;
-        if(evencc >= oddcc){
-            parity =0 ;
-        }else{
-            parity =1 ;
-        }
-        for(int i= 0 ; i < n ; i++){
-            if(nums1[i] % 2 != parity  ){
-                if((nums1[i] -mini) %2 !=parity){
-                    exp = false ;
-                    return exp ;
+        if(min_even == INT_MAX  || min_odd == INT_MAX) return 1 ;
+        
+        for(auto num : nums1){
+            if(num % 2 ==0){
+                if(min_odd > num){
+                    return 0 ; 
                 }
             }
         }
-        return exp ; 
+        return 1 ; 
     }
 };
