d1f10da54e882a61b6925868811a6d7665bacb1d
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-05 21:34:52 +0530

    Time: 9 ms (63.62%), Space: 202.5 MB (96.72%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=  nums.size() ;
-        vector<int> maxprefix(n) ;
+        
         vector<int> minsuffix(n) ; 
-        maxprefix[0] = nums[0] ;
         minsuffix[n-1] = nums[n-1] ;
         int maxi= nums[0] , mini =nums[n-1] ;
+
         for(int i= 1 ; i < n ; i++){
-            maxi  = max(nums[i] , maxi ) ;
             mini = min(nums[n-i-1] , mini) ;
-            maxprefix[i] = maxi ; ;
             minsuffix[n-i-1] = mini;
         }
         for(int i= 0 ; i<n ; i++){
-            int diff = maxprefix[i] - minsuffix[i] ;
+            maxi = max(maxi , nums[i]) ;
+            int diff =maxi- minsuffix[i] ;
             if(diff <= k){
                 return i ;
             }
