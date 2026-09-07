4a1fd8f04357e7f5cc1c52ed11e54eefb1c0e35c
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-07 19:23:54 +0530

    Time: 20 ms (82.65%), Space: 60.3 MB (85.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,21 +5,19 @@ public:
             return nums.size();
         }
         sort(nums.begin() , nums.end()) ;
-        int maxi = 0 ;
+        int maxi = 1;
         int cur = nums[0] ;
         int len = 1; 
         for(int i= 0 ; i < nums.size()-1 ; i++){
             if(nums[i+1] - nums[i] ==1){
                 len++ ; 
-                maxi =max(len , maxi) ;
             }
-            else if(nums[i+1] - nums[i] <1){
-                maxi =max(len , maxi) ;
+            else if(nums[i+1] - nums[i] < 1){
                 continue ;
             }else{
-                maxi =max(len , maxi) ;
                 len =1  ; 
             }
+            maxi =max(len , maxi) ;
             
         }
         return maxi ; 
