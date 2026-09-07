483cbfa82125ece04a1b54f8b4c5fb226e8d5265
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-07 19:24:30 +0530

    Time: 11 ms (95.92%), Space: 60.1 MB (93.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -10,14 +10,19 @@ public:
         int len = 1; 
         for(int i= 0 ; i < nums.size()-1 ; i++){
             if(nums[i+1] - nums[i] ==1){
+
                 len++ ; 
+                maxi =max(len , maxi) ;
             }
             else if(nums[i+1] - nums[i] < 1){
+                maxi =max(len , maxi) ;
                 continue ;
             }else{
+                maxi =max(len , maxi) ;
                 len =1  ; 
+                
             }
-            maxi =max(len , maxi) ;
+            
             
         }
         return maxi ; 
