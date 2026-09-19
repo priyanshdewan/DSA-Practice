416973be757713a64abb5b7c49fd2a47a393f775
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-19 19:21:39 +0530

    Time: 2487 ms (5.03%), Space: 72.2 MB (60.73%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> result(n, 0) ;
-        for(auto booking : bookings){
+        for(auto &booking : bookings){
             for(int i = booking[0]-1 ; i <= booking[1]-1 ; i++){
                 result[i]+= booking[2] ;
             }
