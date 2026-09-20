e448b2b3f6f8ca9d8ce8b196e08553e0ae27f2f0
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-20 12:55:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int n= s.size() ;
         int result  = 0 ; 
-        for(int i =0 ; i<n ; i++){
-            cout << abs(s[i] - 'z') +1;
-            int val = (abs(s[i] - 'z') +1 )*  (i+1) ;
-            result += val ;
+        for(int i =0 ; i<s.size() ; i++){
+            result += (abs(s[i] - 'z') +1 )*  (i+1)  ;
         }
         return result  ;
     }
