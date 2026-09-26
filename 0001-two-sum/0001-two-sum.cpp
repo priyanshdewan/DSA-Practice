6270064444c81dee9bb1ed30e57886d33408ab78
Priyansh Dewan <priyanshdewan18082007@gmail.com>
2026-09-26 11:33:28 +0530

    Time: 2 ms (73.18%), Space: 14.8 MB (44.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+         ios::sync_with_stdio(0);cin.tie(0);
     unordered_map<int,int> m;
 
     for(int i=0;i<nums.size();i++)
